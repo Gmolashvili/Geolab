ef07d3ebe6d3c880ee231794bd2e3bcde7ab26f7
Gmolashvli <gmolashvili15@gmail.com>
2021-03-10 10:27:47 +0400

    Geolab

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,51 @@
+// კალათა
+
+import React from 'react';
+import { Container, Typography, Button, Grid } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+
+import CartItem from './CartItem/CartItem';
+import useStyles from './styles';
+
+const Cart = ({ cart, onUpdateCartQty, onRemoveFromCart, onEmptyCart }) => {
+  const classes = useStyles();
+
+  const handleEmptyCart = () => onEmptyCart();
+
+  const renderEmptyCart = () => (
+    <Typography variant="subtitle1">შენ არ გაქვს დამატებული პროდუქტი კალათაში,
+      <Link className={classes.link} to="/"> დაამატე პროდუქტი</Link>!
+    </Typography>
+  );
+
+  if (!cart.line_items) return 'Loading';
+
+  const renderCart = () => (
+    <>
+      <Grid container spacing={3}>
+        {cart.line_items.map((lineItem) => (
+          <Grid item xs={12} sm={4} key={lineItem.id}>
+            <CartItem item={lineItem} onUpdateCartQty={onUpdateCartQty} onRemoveFromCart={onRemoveFromCart} />
+          </Grid>
+        ))}
+      </Grid>
+      <div className={classes.cardDetails}>
+        <Typography variant="h5">ღირებულება: {cart.subtotal.formatted_with_code}</Typography>
+        <div>
+          <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary" onClick={handleEmptyCart}>კალათის გასუფთავება</Button>
+          <Button className={classes.checkoutButton} component={Link} to="/checkout" size="large" type="button" variant="contained" color="primary">გადახდა</Button>
+        </div>
+      </div>
+    </>
+  );
+
+  return (
+    <Container>
+      <div className={classes.toolbar} />
+      <Typography className={classes.title} variant="h5" gutterBottom>შენი კალათა</Typography>
+      { !cart.line_items.length ? renderEmptyCart() : renderCart() }
+    </Container>
+  );
+};
+
+export default Cart;
---
+++ src/components/Cart/CartItem/styles.js
@@ -0,0 +1,22 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+  media: {
+    height: 260,
+  },
+  cardContent: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  media: {
+    height: 0,
+    paddingTop: '100%',
+  },
+  cartActions: {
+    justifyContent: 'space-between',
+  },
+  buttons: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+}));
